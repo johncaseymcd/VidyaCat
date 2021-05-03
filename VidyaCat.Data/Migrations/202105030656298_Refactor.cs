c509c7aab0015fd3a19cb107c4977f259923ce17
John Casey McDonough <johncaseymcd@gmail.com>
2021-05-03 02:59:11 -0400

    Applied migration. Now playing: Gotta Go by Chung Ha

---
+++ VidyaCat.Data/Migrations/202105030656298_Refactor.cs
@@ -0,0 +1,16 @@
+﻿namespace VidyaCat.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Refactor : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
